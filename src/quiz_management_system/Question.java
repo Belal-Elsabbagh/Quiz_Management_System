a3cfdcf2aa3e974bc7f09340e718718eb29ab523
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-10-27 23:15:43 +0200

    Quiz & Question Classes
    Added attributes to the Quiz class.
    Added attributes to Question class.
    Created MCQ class inside of Question class to manage attributes and methods easier.
    Created an experimental checkAnswer method. Hasn't been tested yet.

--- src/quiz_management_system/Question.java
+++ src/quiz_management_system/Question.java
@@ -1,14 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package quiz_management_system;
 
-/**
- *
- * @author belsa
- */
 public class Question
 {
-    
+    private int questionID;
+    private String prompt;
+    private double grade;
+    //Java doesn't include structs. Using a class to make MCQ easier.
+    private class choice
+    {
+        private int nChoices;
+        private String[] choices;
+        private String answerKey;
+        
+        public boolean checkAnswer(String inAnswer)
+        {
+            boolean result = false;
+            for(int i = 0; i < nChoices; i++)
+            {
+                if(inAnswer == answerKey)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
 }
--- src/quiz_management_system/Quiz.java
+++ src/quiz_management_system/Quiz.java
@@ -1,14 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package quiz_management_system;
 
-/**
- *
- * @author belsa
- */
+import java.util.Calendar;
+
 public class Quiz
 {
+    private int quizID;
+    private String quizTitle;
+    private int nAttempts, nQuestions;
+    private Calendar openTime, duration;
+    private Question[] questionBank;
     
 }
