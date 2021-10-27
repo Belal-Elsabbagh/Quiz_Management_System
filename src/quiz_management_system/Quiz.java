6e6b6b9ec050cef5424b1f4904affd3851e9a7fc
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-10-28 00:01:51 +0200

    Quiz Class updates
    IAdded Test method for for quiz model generator.
    Added setters and getters for main attributes.

--- src/quiz_management_system/Quiz.java
+++ src/quiz_management_system/Quiz.java
@@ -1,6 +1,7 @@
 package quiz_management_system;
 
 import java.util.Calendar;
+import java.util.Random;
 
 public class Quiz
 {
@@ -10,4 +11,66 @@ public class Quiz
     private Calendar openTime, duration;
     private Question[] questionBank;
     
+    public Question[] generateQuizModel()
+    {
+        Question[] newModel;
+        newModel = new Question[nQuestions];
+        
+        for (int i = 0; i < nQuestions; i++)
+        {
+            int rnd = new Random().nextInt(questionBank.length);
+            newModel[i] = questionBank[rnd];
+        }
+        return newModel;
+    }
+
+    public String getQuizTitle()
+    {
+        return quizTitle;
+    }
+
+    public void setQuizTitle(String quizTitle)
+    {
+        this.quizTitle = quizTitle;
+    }
+
+    public int getnAttempts()
+    {
+        return nAttempts;
+    }
+
+    public void setnAttempts(int nAttempts)
+    {
+        this.nAttempts = nAttempts;
+    }
+
+    public int getnQuestions()
+    {
+        return nQuestions;
+    }
+
+    public void setnQuestions(int nQuestions)
+    {
+        this.nQuestions = nQuestions;
+    }
+
+    public Calendar getOpenTime()
+    {
+        return openTime;
+    }
+
+    public void setOpenTime(Calendar openTime)
+    {
+        this.openTime = openTime;
+    }
+
+    public Calendar getDuration()
+    {
+        return duration;
+    }
+
+    public void setDuration(Calendar duration)
+    {
+        this.duration = duration;
+    }
 }
