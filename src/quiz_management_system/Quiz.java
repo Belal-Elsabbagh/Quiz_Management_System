d67920528d41bc03711f7cefe82538555495fa92
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-10-28 09:35:41 +0200

    New Quiz create and display methods
    Added create and display functions for Quiz Class. Did not find a way to set and get the openTime and duration yet.
    Changed the generateQuizModel algorithm.
    Created a main function to test creating and displaying a quiz.
    The quiz class works properly without the question bank or the openTime and duration attributes.

--- src/quiz_management_system/Quiz.java
+++ src/quiz_management_system/Quiz.java
@@ -2,24 +2,52 @@ package quiz_management_system;
 
 import java.util.Calendar;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Quiz
 {
+    private static int count = 0;
     private int quizID;
     private String quizTitle;
     private int nAttempts, nQuestions;
-    private Calendar openTime, duration;
+    private Calendar openTime, duration; //still uncertain of the data type.
     private Question[] questionBank;
+
+    public Quiz()
+    {
+        this.quizID = ++count;
+    }
+     
+    public void display()
+    {
+        System.out.println("-----------Displaying quiz properties------------");
+        System.out.println("Quiz ID: " + quizID);
+        System.out.println("Quiz Title: " + quizTitle);
+        System.out.println("Number of questions: " + nQuestions);
+        System.out.println("Number of attempts: " + nAttempts);
+        System.out.println("-------------------------------------------------");
+    }
+    
+    public void create()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-----------Creating a new quiz-------------------");
+        System.out.println("Enter quiz title: "); quizTitle = sc.next();
+        System.out.println("Enter number of questions: "); nQuestions = sc.nextInt();
+        System.out.println("Enter number of attempts: "); nAttempts = sc.nextInt();
+    }
     
     public Question[] generateQuizModel()
     {
         Question[] newModel;
         newModel = new Question[nQuestions];
         
+        Random generator = new Random();
+        int randIndex;
         for (int i = 0; i < nQuestions; i++)
         {
-            int rnd = new Random().nextInt(questionBank.length);
-            newModel[i] = questionBank[rnd];
+            randIndex = generator.nextInt(questionBank.length);
+            newModel[i] = questionBank[randIndex];
         }
         return newModel;
     }
--- src/quiz_management_system/Quiz_Management_System.java
+++ src/quiz_management_system/Quiz_Management_System.java
@@ -1,22 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package quiz_management_system;
 
-/**
- *
- * @author belsa
- */
+import java.util.Scanner;
+
 public class Quiz_Management_System
 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        testCreateQuiz();
     }
-    
+    public static void testCreateQuiz()
+    {
+        Scanner sc = new Scanner(System.in);
+        Quiz q1;
+        q1 = new Quiz();
+        q1.create();
+        
+        q1.display();
+    }
 }
