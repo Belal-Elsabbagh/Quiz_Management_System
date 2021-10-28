05e23e4efdb14de151ee3ef3d4217736a36db65f
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-10-28 19:40:43 +0200

    User and Teacher classes
    Added attributes to user class.
    Added an ID generator in the user constructor.
    Added Teacher class.
    Added an INCOMPLETE main menu for the teacher.

---
+++ src/quiz_management_system/Teacher.java
@@ -0,0 +1,11 @@
+package quiz_management_system;
+
+public class Teacher
+{
+    public void listMainMenu()
+    {
+        System.out.println("----------Teacher Operations Main Menu-----------");
+        System.out.println("1. Open Quiz.");
+        System.out.println("2. List my quizzes.");
+    }
+}
--- src/quiz_management_system/User.java
+++ src/quiz_management_system/User.java
@@ -2,5 +2,13 @@ package quiz_management_system;
 
 public class User
 {
-    
+    private static int count = 0;
+    private int userID;
+    private String username;
+    private String password;
+
+    public User()
+    {
+        this.userID = ++count;
+    }
 }
