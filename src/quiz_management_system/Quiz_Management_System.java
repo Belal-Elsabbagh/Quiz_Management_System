1886217ccbde24ad10e6c823529e93786af4294f
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-11-02 14:05:48 +0200

    Update Quiz_Management_System.java

--- src/quiz_management_system/Quiz_Management_System.java
+++ src/quiz_management_system/Quiz_Management_System.java
@@ -1,6 +1,7 @@
 package quiz_management_system;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Quiz_Management_System
 {
@@ -12,6 +13,32 @@ public class Quiz_Management_System
     public static void main(String[] args)
     {
         testCreateQuiz();
+        Scanner sc = new Scanner(System.in);
+        String inUsername, inPassword;
+        
+        System.out.println("Enter username:");
+        inUsername = sc.next();
+        System.out.println("Enter password:");
+        inPassword = sc.next();
+        User newUser = new User(inUsername, inPassword);
+        int status = -1;
+        status = newUser.checkLogin();
+        
+        if(status == 0)
+            System.out.println("User not found.");
+        else if(status == 1)
+            System.out.println("Incorrect password.");
+        else if(status == 2)
+        {
+            if(newUser.getAccessLevel() == 0)
+            {
+                Student activeStudent = newUser.convert2Student();
+            }
+            else if(newUser.getAccessLevel() == 1)
+            {
+                Teacher activeTeacher = newUser.convert2Teacher();
+            }
+        }
     }
     public static void testCreateQuiz()
     {
