199b5bf5cf001d92fc4d51dfdd2643b2394fa57a
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-12-09 00:36:24 +0200

    Code and Algorithm optimizations.
    Implemented a single login function (on the hopes that the ArrayList will store the data correctly.
    Removed all login functions on the hopes that a unified login will work.
    Updated the DataHandler to have static data structures to access from anywhere instead of passing by reference.
    Updated all references of DataHandler members to accomodate for the static changes.
    Added interface for console menu function.
    Optimized conditions to not need else as much as possible.

--- src/quiz_management_system/Admin.java
+++ src/quiz_management_system/Admin.java
@@ -7,90 +7,45 @@ import java.util.Scanner;
  * @author belsa
  */
 public class Admin extends User
-{  
-    public static Admin adminLogin(DataHandler data)
+{
+    public Admin(String inUsername, String inPassword)
+    {
+        super(inUsername, inPassword, 3);
+    }
+
+    public void createUser(DataHandler data)
     {
         Scanner sc = new Scanner(System.in);
-        String inUsername, inPassword;
+        String inUsername, inPassword = "";
+        int aLevel;
 
-        System.out.println("Enter username:");
-        inUsername = sc.next();
+        System.out.println("-----------Creating new user------------");
+        System.out.println("Enter User");
+        System.out.println("Enter username:"); inUsername = sc.next();
+        User newUser = new User(inUsername, inPassword);
+
         System.out.println("Enter password:");
         inPassword = sc.next();
 
-        Admin newAdmin = null;
-        for (Admin i : data.adminData)
-        {
-            if (inUsername.equals(i.getUsername()))
-            {
-                if (inPassword.equals(i.getPassword()))
-                {
-                    newAdmin = i;
-                    System.out.println("Login Successful");
-                    break;
-                }
-                else
-                {
-                    System.err.println("Incorrect password.");
-                    break;
-                }
-            }
-            else
-            {
-                System.err.println("User not found.");
-            }
-        }
-        return newAdmin;
-    }
-        
-    public void createUser(DataHandler data)
-    {        
-        System.out.println("-----------Creating new user------------");
-        Scanner sc = new Scanner(System.in);
-        
-        String inUsername = new String(), inPassword = new String();
-        int aLevel;
-        User newUser = null;
-
-        
-        int status = 0;
-        do
-        {
-            System.out.println("Enter username:");
-            inUsername = sc.next();
-            newUser = new User(inUsername, inPassword);
-            
-            if(data.studentData.contains(newUser) || data.teacherData.contains(newUser))
-            {
-                System.err.println("Username already exists.");
-                continue;
-            }
-            System.out.println("Enter password:");
-            inPassword = sc.next();
-        }while(status == 1);
-        
-
-        System.out.println("Set Access Level.\n 1 for student\n2 for teacher:");
+        System.out.println("Set Access Level.\n 1 for student\n2 for teacher\n3 for admin:");
         aLevel = sc.nextInt();
         if(aLevel == 1)
         {
-            //newUser = new Student(inUsername, inPassword, aLevel);
-            newUser.setUserID(1*1000 + data.studentData.size() + 1);
+            newUser = new Student(inUsername, inPassword);
+            newUser.setUserID(10000 + data.studentData.size() + 1);
             data.studentData.add((Student)newUser);
         }
         else if(aLevel == 2)
         {
-            //newUser = new Teacher(inUsername, inPassword, aLevel);
-            newUser.setUserID(2*1000 + data.teacherData.size() + 1);
+            newUser = new Teacher(inUsername, inPassword);
+            newUser.setUserID(20000 + data.teacherData.size() + 1);
             data.teacherData.add((Teacher)newUser);
         }
-    }
-    public void removeUser(DataHandler data)
-    {
-        
-    }
-    public void editUser(DataHandler data)
-    {
-        
+        else if(aLevel == 3)
+        {
+            newUser = new Admin(inUsername, inPassword);
+            newUser.setUserID(30000 + data.adminData.size() + 1);
+            data.adminData.add((Admin)newUser);
+        }
     }
 }
--- src/quiz_management_system/Quiz_Management_System.java
+++ src/quiz_management_system/Quiz_Management_System.java
@@ -1,15 +1,14 @@
 package quiz_management_system;
 
 import java.text.ParseException;
-import java.time.LocalTime;
 import java.util.Scanner;
-
-import static quiz_management_system.Student.studentLogin;
-import static quiz_management_system.Teacher.teacherLogin;
-import static quiz_management_system.Admin.adminLogin;
 
 public class Quiz_Management_System
 {
+    private static User activeUser;
+
+    public static User getActiveUser() { return activeUser;  }
+
     public static void main(String[] args) throws ParseException
     {
         DataHandler data = new DataHandler();
@@ -21,36 +20,13 @@ public class Quiz_Management_System
 
     public static void consoleLogin() throws ParseException
     {
-        DataHandler data = new DataHandler();
-
-        LocalTime date = LocalTime.now();
-        System.out.println(date);
-        System.out.println("*****Quiz Management System*****");
-        System.out.println("Enter user:\n1. Student.\n2. Teacher.\n3. Admin.");
         Scanner sc = new Scanner(System.in);
-        int type = sc.nextInt();
-
-        if (type == 1)
-        {
-            Student activeS = studentLogin();
-            if (activeS == null)
-                System.err.println("Login unsuccessful.");
-            else
-                activeS.listStudentMenu();
-        }
-        else if (type == 2)
-        {
-            Teacher activeT = teacherLogin();
-            if (activeT == null)
-                System.err.println("Login unsuccessful.");
-            else
-                activeT.listTeacherMenu();
-        }
-        else if (type == 3)
-        {
-            Admin activeA = adminLogin();
-            if (activeA == null)
-                System.err.println("Login unsuccessful.");
-        }
+        String inUsername, inPassword;
+        System.out.println("*****Quiz Management System*****");
+        System.out.println("Enter username:"); inUsername = sc.next();
+        System.out.println("Enter password:"); inPassword = sc.next();
+        activeUser = User.login(inUsername, inPassword);
+        while (activeUser != null)
+            activeUser.listMenu();
     }
 }
