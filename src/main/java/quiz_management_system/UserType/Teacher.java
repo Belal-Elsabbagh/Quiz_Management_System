6f9fe8db9a980ddadfab47f93a8ec502c0909627
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-12-19 23:23:25 +0200

    GUI Changes. Moved CreateQuestionBank to Quiz.java. Added ReviewQuizGrades to Teacher.java. Added DoAttemptWindow to Student.java. Added ReviewAttemptWindow to Student.java

--- src/main/java/quiz_management_system/UserType/Teacher.java
+++ src/main/java/quiz_management_system/UserType/Teacher.java
@@ -149,135 +149,84 @@ public class Teacher extends User
 
     }
 
-    static class CreateQuestionBank extends JFrame
+    /**
+     * @author Habiba1234567
+     */
+    static class ReviewQuizGrades extends JFrame
     {
-
         static JFrame window;
-        static JPanel Back, Title, down;
+        static JPanel title_panel, background;
         static Font myFont;
-        static JLabel Title_label, prompt_label, grade_label, choice_label, c1, c2, c3, c4, index_label, Q;
-        static JButton right_b, left_b;
-        static Icon icon_r, icon_l;
-        static JTextField Q_t, G_t, c1_t, c2_t, c3_t, c4_t, I_t;
+        static JLabel Title, label1, label2, label3, label4, label5, label6;
         static Border brdr;
+        static JTable table;
+        static String[] columnNames = {"Student", "Grade"};
+        static Object[][] data = {
+                {"Kathy", 5}, {"John", 4}};
+        static JScrollPane scrollpane;
 
         static
         {
-            myFont = new Font(Font.MONOSPACED, Font.BOLD, 30);
-            Title_label = new JLabel("Question Creator");
-            Back = new JPanel();
-            Title = new JPanel();
+            title_panel = new JPanel();
             brdr = BorderFactory.createLineBorder(new Color(222, 184, 150));
-            down = new JPanel();
-            Q = new JLabel("Question number: " + 1);
-            prompt_label = new JLabel("Enter prompt:");
-            grade_label = new JLabel("Enter grade: ");
-            choice_label = new JLabel("Enter choices: ");
-            index_label = new JLabel("Enter the index of right answer: ");
-            c1 = new JLabel("1.");
-            c2 = new JLabel("2.");
-            c3 = new JLabel("3.");
-            c4 = new JLabel("4.");
-            right_b = new JButton();
-            left_b = new JButton();
-            icon_r = new ImageIcon("UR.PNG");
-            icon_l = new ImageIcon("UL.PNG");
-            Q_t = new JTextField();
-            G_t = new JTextField();
-            c1_t = new JTextField();
-            c2_t = new JTextField();
-            c3_t = new JTextField();
-            c4_t = new JTextField();
-            I_t = new JTextField();
+            background = new JPanel();
+            myFont = new Font(Font.MONOSPACED, Font.BOLD, 30);
+            Title = new JLabel("Review Quiz Grades ");
+            label1 = new JLabel("Best Grade : ");
+            label2 = new JLabel("Worst Grade : ");
+            label3 = new JLabel("Average Grade : ");
+            label4 = new JLabel(" ");
+            label5 = new JLabel(" ");
+            label6 = new JLabel(" ");
+            table = new JTable(data, columnNames);
+            scrollpane = new JScrollPane(table);
+            table.setFillsViewportHeight(true);
         }
 
-        public CreateQuestionBank()
+        public ReviewQuizGrades()
         {
-            //Title
-            Title.add(Title_label);
-            add(Title, BorderLayout.PAGE_START);
-            Title_label.setFont(myFont);
-            Title_label.setForeground(Color.BLACK);
-            Title.setBackground(Color.WHITE);
-            Title.setBorder(brdr);
-            // right/left buttons
-            right_b.setIcon(icon_r);
-            left_b.setIcon(icon_l);
-            right_b.setBackground(Color.WHITE);
-            left_b.setBackground(Color.WHITE);
-            down.add(left_b, BorderLayout.EAST);
-            down.add(right_b, BorderLayout.PAGE_END);
-            down.setBackground(Color.WHITE);
-            add(down, BorderLayout.PAGE_END);
+            Title.setFont(myFont);
+            title_panel.add(Title);
+            add(title_panel, BorderLayout.PAGE_START);
+            Title.setForeground(Color.BLACK);
+            title_panel.setBackground(Color.white);
+            title_panel.setBorder(brdr);
+
             //labels
-            Q.setBounds(5, 60, 200, 30);
-            prompt_label.setBounds(5, 80, 100, 30);
-            grade_label.setBounds(5, 150, 100, 30);
-            choice_label.setBounds(5, 180, 100, 30);
-            index_label.setBounds(5, 380, 200, 30);
-            c1.setBounds(5, 220, 10, 10);
-            c2.setBounds(5, 260, 10, 10);
-            c3.setBounds(5, 300, 10, 10);
-            c4.setBounds(5, 340, 10, 10);
-            add(Q);
-            add(prompt_label);
-            add(grade_label);
-            add(choice_label);
-            add(index_label);
-            add(c1);
-            add(c2);
-            add(c3);
-            add(c4);
-            //text fields
-            Q_t.setBounds(5, 110, 330, 30);
-            G_t.setBounds(100, 150, 60, 30);
-            c1_t.setBounds(30, 210, 300, 30);
-            c2_t.setBounds(30, 250, 300, 30);
-            c3_t.setBounds(30, 290, 300, 30);
-            c4_t.setBounds(30, 330, 300, 30);
-            I_t.setBounds(210, 380, 60, 30);
-            add(Q_t);
-            add(G_t);
-            add(c1_t);
-            add(c2_t);
-            add(c3_t);
-            add(c4_t);
-            add(I_t);
-            //background
-            Back.setBackground(Color.WHITE);
-            add(Back, BorderLayout.CENTER);
-        }
+            label1 = new JLabel("Title: ");
+            label1.setBounds(10, 100, 60, 30);
+            label2 = new JLabel("Best Grade : ");
+            label2.setBounds(10, 140, 100, 30);
+            label3 = new JLabel("Worst Grade : ");
+            label3.setBounds(10, 180, 200, 30);
+            label4 = new JLabel("Average Grade : ");
+            label4.setBounds(10, 220, 200, 30);
+            label5 = new JLabel(" ");
+            label5.setBounds(10, 260, 60, 30);
+            label6 = new JLabel(" ");
+            label6.setBounds(10, 300, 60, 30);
+            add(label1);
+            add(label2);
+            add(label3);
+            add(label4);
+            add(label5);
+            add(label6);
 
-        public static void constructWindow()
-        {
-            window = new CreateQuestionBank();
-            window.setTitle("Create Question Bank");
-            window.setSize(550, 550);
-            window.setResizable(false);
-            window.setLocationRelativeTo(null); // to not have it open at the corner
-            window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            window.setVisible(true);
-        }
-    }
+            //Background
+            background.setBackground(Color.WHITE);
+            add(background, BorderLayout.CENTER);
 
-    static class ReviewQuizWindow extends JFrame
-    {
-        static JFrame window;
-
-        static
-        {
-
-        }
-
-        public ReviewQuizWindow()
-        {
+            //Table
+            table.setBounds(100, 300, 300, 500);
+            table.setBackground(new Color(222, 184, 150));
+            add(table);
 
         }
 
         public static void constructWindow()
         {
-            window = new ReviewQuizWindow();
-            window.setTitle("Logged in as " + Quiz_Management_System.getActiveUser().getUsername());
+            window = new ReviewQuizGrades();
+            //  window.setTitle("Logged in as " + Quiz_Management_System.getActiveUser().getUsername());
             window.setSize(400, 200);
             window.setLocationRelativeTo(null); // to not have it open at the corner
             window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
