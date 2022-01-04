0ace2bf3f0660d4ef10f29d3d1f0f180a2a88e6a
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-01-04 22:46:53 +0200

    Added Client class

--- src/main/java/quiz_management_system/Server.java
+++ src/main/java/quiz_management_system/Server.java
@@ -1,24 +1,20 @@
 package quiz_management_system;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server extends JFrame implements Runnable
+public class Server implements Runnable
 {
     ServerSocket skt;
-    
+
     static Socket s;
     static DataInputStream dataIn;
     static DataOutputStream dataOut;
 
-    JTextArea chat;
-
-    public Server() throws HeadlessException
+    public Server()
     {
         try
         {
@@ -38,9 +34,7 @@ public class Server extends JFrame implements Runnable
             {
                 s = skt.accept();
                 dataIn = new DataInputStream(s.getInputStream());
-                chat.setText(chat.getText() + dataIn.readUTF());
                 dataOut = new DataOutputStream(s.getOutputStream());
-                dataOut.writeUTF(chat.getText());
             }
         } catch (IOException e)
         {
